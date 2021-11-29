0eeb23b301367b19948f05cc49d04ffe0bb4e2cc
seunawonugba <seun.awonugba@decagon.dev>
2021-11-29 15:41:17 +0100

    implement trigger

--- app/src/main/java/com/example/userscountry/MainActivity.kt
+++ app/src/main/java/com/example/userscountry/MainActivity.kt
@@ -4,18 +4,25 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.telephony.TelephonyManager
+import android.widget.Button
 import android.widget.TextView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var countryCode : TextView
+    private lateinit var button : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         countryCode = findViewById(R.id.countryCodeTextView)
+        button = findViewById(R.id.button)
 
-        countryCode.text = "Users country code: ${getUserCountry(this)}"
+        button.setOnClickListener {
+            countryCode.text = getUserCountry(this)
+        }
+
+
 
 
     }
@@ -25,12 +32,12 @@ class MainActivity : AppCompatActivity() {
             val tm = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
             val simCountry = tm.simCountryIso
             if (simCountry != null && simCountry.length == 2) { // SIM country code is available
-                return simCountry.toLowerCase(Locale.US)
+                return simCountry.uppercase(Locale.US)
             }
             else if (tm.phoneType != TelephonyManager.PHONE_TYPE_CDMA) { // Device is not 3G (would be unreliable)
                 val networkCountry = tm.networkCountryIso
                 if (networkCountry != null && networkCountry.length == 2) { // network country code is available
-                    return networkCountry.toLowerCase(Locale.US)
+                    return networkCountry.uppercase(Locale.US)
                 }
             }
         }
